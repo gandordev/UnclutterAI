8deca7aa5db9f5813fa0b7746adbdb56350913d7
gandordev <grlgh353@gmail.com>
2026-01-18 00:22:23 +0100

    Fix bugs, add new rule and docstrings

--- unclutter.js
+++ unclutter.js
@@ -10,6 +10,7 @@ const ELEMENTS_TO_HIDE = [
     '[data-testid="sidebar-upgrade-button"]', // subscription btn (left)
     '[class*="gap-xs flex flex-col items-center"]', // install browser btn (left)
     'div.gap-sm.flex.col-start-1.row-start-2.-ml-two', // prompt input btns
+    'div.bottom-md.rounded-lg.border.ease-out.bg-base', // download app btn
 
     /* Gemini */
     'div.right-section' // try Google AI Plus
@@ -17,20 +18,38 @@ const ELEMENTS_TO_HIDE = [
 const EVENTS = ["change", "cuechange"]
 
 
-// Add observer
-const observer = new MutationObserver(hideElements);
+/**
+ * Watch for changes being made to the DOM tree.
+ * @param {MutationCallback} deleteElements Defines what shoud happen
+ * @returns {void}
+ */
+const observer = new MutationObserver(deleteElements);
 observer.observe(document.body, {
     childList: true,
     subtree: true,
+    attributeOldValue: true,
 });
 
 // Add listener to changing events
-EVENTS.forEach(event => {
-    document.addEventListener(event, () => { hideElements(); });
+window.addEventListener('onload', () => {
+    EVENTS.forEach(event => {
+        document.addEventListener(event, () => { deleteElements(); });
+    });
 });
 
 
-function hideElements() {
+/**
+ * Delete DOM elements.
+ * @returns {void}
+ */
+function deleteElements() {
+    // Enable scrolling (when using chatgpt without signing in)
+    let loggedOutOption = document.querySelector('a.text-token-text-secondary.mt-5.cursor-pointer.text-sm.font-semibold.underline')
+    if (document.URL.includes("chatgpt") && loggedOutOption) {
+        unlockScrollBody(loggedOutOption);
+        return; // Avoid to delete a non-existent element
+    }
+
     try {
         ELEMENTS_TO_HIDE.forEach(selector => {
             const tag = document.querySelector(selector);
@@ -40,23 +59,18 @@ function hideElements() {
     catch (e) {
         console.error(`Error al ocultar elemento: ${e}`);
     }
-
-    // Enable scrolling (when using chatgpt without signing in)
-    if (document.URL.includes("chatgpt")) {
-        unlockScrollBody();
-    }
 }
 
-function unlockScrollBody() {
+/**
+ * Checks if we are using ChatGPT without logging in and if so
+ * auto clicks on "Stay logged out" button to unlock scrolling.
+ * @param {Element} loggedOutOption
+ * @returns {void}
+ */
+function unlockScrollBody(loggedOutOption) {
     const body = document.querySelector('body');
     const scrollLocked = body?.hasAttribute('data-scroll-locked');
     if (scrollLocked) {
-        // body.removeAttribute('data-scroll-locked');
-        // body.style.removeProperty('pointer-events');
-
-        // document.documentElement.style.overflow = '';
-        // document.body.style.overflow = '';
-
         document.querySelector('a.text-token-text-secondary.mt-5.cursor-pointer.text-sm.font-semibold.underline')
         .click();
     }
